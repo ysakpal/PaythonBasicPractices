375f5ae56f4a1212c1502bf5719ba9157816b5b1
harig531 <harig531@gmail.com>
2019-09-11 12:44:32 +0530

    new files is add

---
+++ Patterns.py
@@ -0,0 +1,15 @@
+row = int(input("Please enter input number"))
+for j in range(0,row+1):
+    for cols in range(0,j+1):
+        print("*" , end = " ")
+    print()
+
+    """ 
+    Out put
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    * * * * * *
+    * * * * * * * """
