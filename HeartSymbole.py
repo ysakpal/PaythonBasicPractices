32ae78670691a6fba978521acc94ab91734ae011
Hari Guntu <47913430+harig531@users.noreply.github.com>
2019-09-11 10:59:10 +0530

    Added Conditions wiith this files

--- HeartSymbole.py
+++ HeartSymbole.py
@@ -1,5 +1,7 @@
 for row in range(6):
     for cols in range(7):
-        if cols == 2:
-            print("*",end="")
-    print("")
+        if (row == 0 and cols % 3 != 0) or (row == 1 and cols % 3 == 0) or ( row - cols == 2) or (row + cols == 8):
+            print("*" ,end=" ")
+        else:
+            print(end=" ")
+    print()
